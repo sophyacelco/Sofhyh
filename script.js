70ad55c3835c271def88e4899decce64957de606
sophyacelco <00001092668512sp@al.educacao.sp.gov.br>
2025-09-15 19:14:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,11 +5,16 @@
     botaoDeAcessibilidade.addEventListener('click', function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao')
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')    
-    })
 
+    const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+    botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+
+    })
 
     const aumentaFonteBotao = document.getElementById('Aumentar-Fonte');
     const diminuiFonteBotao = document.getElementById('Diminuir-Fonte');
+
+    const alternaContraste = document.getElementById('Alterna-Contraste');
 
     let tamanhoAtualFonte = 1;
 
@@ -22,4 +27,14 @@
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
      })
+
+     alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+     })
  })
+
+ScrollReveal().reveal('#inicio',{dalay: 500});
+ScrollReveal().reveal('#tropicalia',{dalay: 500});
+ScrollReveal().reveal('#galeria',{dalay: 500});
+ScrollReveal().reveal('#contato',{dalay: 500});
+ 
